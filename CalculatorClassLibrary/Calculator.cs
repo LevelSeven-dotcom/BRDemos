43454253a2e4cb8a390dcdb7ebee6b62ac2d61dd
leonard <vinyasleonard.lobo@broadridge.com>
2025-04-29 05:36:04 +0000

    WebAPI1

---
+++ CalculatorClassLibrary/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using CalculatorClassLibrary.Exceptions;
+
+namespace CalculatorClassLibrary
+{
+
+    //calculator should find the sun if two non-zero numbers, non-negative, non-odd ints and return the rsult else throw expection 
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+                throw new ZeroInputException("numbers must be non zero");
+
+            if (a < 0 || b < 0)
+                throw new NegativeInputException("numbers should be non negative");
+
+            if (a % 2 != 0 || b % 2 != 0)
+                throw new OddInputException("numbers must be non odd");
+
+            return a + b;
+        }
+    }
+}
